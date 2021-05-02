f8c580b9a5e42992fa8fd6f1a9e20cd814bb676e
Christopher Tan <christopher.tan17@gmail.com>
2021-05-02 15:40:26 -0700

    added sass, basic styling, logo

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import '../styles/navbar.scss';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav className="container">
+        <img src={logo} className="Logo" alt="on track logo" />
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
