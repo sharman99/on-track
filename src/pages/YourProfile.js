228bb114b687b8180b4fee7c5c42514e28f3eaad
Ruth-Ann Armstrong <ruthanna@stanford.edu>
2021-05-16 05:44:09 -0700

    your profile page loads info from database, refresh issue fixed, ratings images added (not loaded onto page yet though), missing fields to be captured when signing up added:

--- src/pages/YourProfile.js
+++ src/pages/YourProfile.js
@@ -3,6 +3,14 @@ import TextField from '@material-ui/core/TextField';
 import React, { Component } from 'react';
 import firebase from '../firebase';
 import your_profile_img from "../media/your_profile_img.png";
+import onestar from "../media/1s.png";
+import twostar from "../media/2s.png";
+import threestar from "../media/3s.png";
+import fourstar from "../media/4s.png";
+import fivestar from "../media/5s.png";
+import zerostar from "../media/0s.png";
+import unrated from "../media/unrated.png";
+
 import Person from "../media/profile.json";
 
 class YourProfile extends Component{
@@ -18,28 +26,36 @@ class YourProfile extends Component{
     if(localStorage.getItem('curr_email') == null && (this.props.location.state == undefined || this.props.location.state.signed_in_email == undefined || this.props.location.state.signed_in_email  == null)){
       console.log("rerouting to sign in")
       this.props.history.push('/sign_in');
+      console.log("fail")
+  
     }
     else{
       //this is how you recieve the props variable sent from the last page
-      if (localStorage.getItem('curr_email') == null){
+      if (this.props.location.state != undefined  && this.props.location.state.signed_in_email != null ){
         localStorage.setItem( 'curr_email', this.props.location.state.signed_in_email );
       }
 
       const db = firebase.firestore();
+      console.log("success")
 
       //get the "a" variable from database 
     //get the "a" variable from database 
     db.collection("userInfo").doc(localStorage.getItem('curr_email') ).get().then((doc) => {
       //setting the state variable called "temp" to the variable in the doc with the name "a"
+      console.log("curr email", localStorage.getItem('curr_email'))
+      
       this.setState({firstname: doc.data().first_name})
       this.setState({lastname: doc.data().last_name})
       this.setState({pronouns: doc.data().pronouns})
       this.setState({major: doc.data().major})
-      this.setState({year: doc.data().pronouns})
-      this.setState({communication: doc.data().pronouns})
-      this.setState({work: doc.data().pronouns})
-      this.setState({hours: doc.data().pronouns})
-      this.setState({goals: doc.data().pronouns})
+      this.setState({year: doc.data().year})
+      this.setState({communication: doc.data().accountability_style})
+      this.setState({work: doc.data().accountability_for})
+      this.setState({hours: doc.data().estimated_hours})
+      this.setState({goals: doc.data().goal})
+      if (doc.data().rating == null){
+        this.setState({rating: unrated})
+      }
 
     })
   }
@@ -55,7 +71,7 @@ class YourProfile extends Component{
       password,
     } = this.state;
     return (
-      <div className="Profile">
+      <div className="My Profile">
         {/*This is how we display state variables. Below we are displaying the state variables called "temp_val" and "cs"*/}
         <div>{this.state.temp_val}</div>
         <div>{this.state.cs}</div>
@@ -63,16 +79,17 @@ class YourProfile extends Component{
           <h1>profile</h1>
           <button className="report-button" onClick={this.reportUser}>report user</button>
         </div>
-        {/* <img src={profile_img} alt="profile picture" /> */}
+        <img class = "prof" src={your_profile_img} alt="profile picture" />
+        <img class = "rate" src={this.state.rating} alt="rating" />
+
         <h2>{this.state.firstname} {this.state.lastname} ({this.state.pronouns})</h2>
         <p>{this.state.major}</p>
-        <p>{Person.year}</p>
-        <p>{Person.communication.join(', ')}</p>
+        <p>{this.state.year}</p>
+        <p>{this.state.communication}</p>
         <h2>additional info</h2>
-        <p>types of work: {Person.work.join(', ')}</p>
+        <p>types of work: {this.state.work}</p>
         <p>hours / week: {this.state.hours}</p>
-        <p>goals: {Person.goals.join(', ')}</p>
-        <p>previous pod rating: {Person.rating}/5</p>
+        <p>goals: {this.state.goals}</p>
       </div>
     );
   }
