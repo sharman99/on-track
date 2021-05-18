5517edabe70262604e3573267d1ddfb5c38af048
azhng77 <azhng77@gmail.com>
2021-05-17 20:06:42 -0400

    guidelines and routing for guidelines

---
+++ src/pages/Guidelines.js
@@ -0,0 +1,21 @@
+import logo from '../logo.svg';
+import React, { Component } from 'react';
+
+
+class Guidelines extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+    }
+  }
+
+  render(){
+    return (
+      <div className="Sheets">
+          <h1> On Track Community Guidelines </h1>
+      </div>
+    );
+  }
+}
+
+export default Guidelines;
